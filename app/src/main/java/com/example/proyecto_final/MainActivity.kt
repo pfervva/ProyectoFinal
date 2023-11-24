37ef141e70e7885b7825c4e8a7721f1192205349
Pedro <pfernandezgonzalez@vva.safanet.es>
2023-11-24 02:52:37 +0100

    Versión 0.3 - Logica para pasar una diapositiva a otra

--- app/src/main/java/com/example/proyecto_final/MainActivity.kt
+++ app/src/main/java/com/example/proyecto_final/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.proyecto_final
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView7: TextView = findViewById(R.id.textView7)
+
+        textView7.setOnClickListener {
+            // Crear un Intent para cambiar a la actividad del layout de inicio de sesión
+            val intent = Intent(this, LoginActivity::class.java)
+
+            // Iniciar la nueva actividad
+            startActivity(intent)
+        }
     }
 }
